d05a53e6fe9c0dcf83d453210e977e666e0609b7
Aman Kumar <aman.kumar@MYREALDATA.RTDS>
2018-08-08 14:21:42 +0530

    getting forms values

--- src/App.js
+++ src/App.js
@@ -1,81 +1,109 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
-class App extends Component {
+class App extends React.Component {
   state = {
-    forms: [],
-    lastNum: 0,
-  }
+    forms: {},
+  };
 
   addForm = () => {
+    const newForm = {
+      id: guid(),
+      i1: "",
+      i2: "",
+      i3: "",
+    }
+    this.setState(prevState => ({
+      forms: { ...prevState.forms, [newForm.id]: newForm },
+    }));
+  };
+
+  removeForm = id => {
+    const forms = { ...this.state.forms };
+    delete forms[id];
+    this.setState({ forms });
+  };
+
+  onChange = ( e ) => {
+    const { id, name, value } = e.target;
     this.setState( prevState => ( {
-      forms: [ ...prevState.forms, prevState.lastNum++ ],
+      forms: { ...prevState.forms, [id]: { ...prevState.forms[id], [name]: value } }
     } ));
+  };
+
+  onSubmit = id => { 
+    const { i1, i2, i3 } = this.state.forms[id];
+    console.log( ` Form id: ${id}, start time: ${i1}, end time: ${i2}, Description: ${i3} ` );
   }
 
-  removeForm = form => {
-    const newForms = this.state.forms.filter( el => el !== form  );
-    this.setState( { forms: newForms } );
-  }
+  renderForms = () => Object.keys(this.state.forms).map(key => (
+    <FormItem
+      key={this.state.forms[key].id}
+      id={this.state.forms[key].id}
+      removeForm={this.removeForm}
+      onChange={this.onChange}
+      onSubmit={this.onSubmit}
+      />
+  ));
 
   render() {
-    const forms = this.state.forms.map( form => (
-      <Test key={form} form={form} removeForm={this.removeForm} />
-    ));
-
     return (
       <div className="App ">
         <div className="App-header App-intro">
           <button className="btn btn-success" onClick={this.addForm}>+</button>
         </div>
-        {forms}
+        {this.renderForms()}
       </div>
     );
   }
-
 }
 
-const Test = ( props ) => {
-  const handleRemove =  e => {
+const FormItem = (props) => {
+  const { id, removeForm, onChange, onSubmit } = props;
+  
+  const handleRemove = () => removeForm(id);
+ 
+  const handleSubmit = e => {
     e.preventDefault();
-    props.removeForm( props.form );
-  }
-
-  const handleSubmit =  (e) => {
-    e.preventDefault();
-    console.log("Submitted values are : " +" "+e.target[0].value);
-    
-  }
+    onSubmit( id );
+  } 
 
   return (
-  <div className="container">
-    <div className="panel-group">
-      <div className="panel panel-primary">
-      <div className="panel-heading">Add Component In your video </div>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label >Start Time:</label>
-              <input type="time" className="form-control" name="start_time" placeholder="Start Time" />
-            </div>
-            <div className="form-group col-md-6">
-              <label >End Time</label>
-              <input type="time" className="form-control" placeholder="End Time" />
-            </div>
-            <div className="form-group col-md-12">
-              <label>Description:</label>
-              <input type="text" className="form-control" placeholder="Add Description About Component" />
-            </div>
+      <div className="container">
+        <div className="panel-group">
+          <div className="panel panel-primary">
+          <div className="panel-heading">Add Component In your video </div>
+            <form onSubmit={handleSubmit}>
+              <div className="form-row">
+                <div className="form-group col-md-6">
+                  <label >Start Time:</label>
+                  <input id={id} onChange={onChange}  name="i1" type="time" className="form-control" placeholder="Start Time" />
+                </div>
+                <div className="form-group col-md-6">
+                  <label >End Time</label>
+                  <input  id={id} onChange={onChange}  name="i2" type="time" className="form-control" placeholder="End Time" />
+                </div>
+                <div className="form-group col-md-12">
+                  <label>Description:</label>
+                  <input  id={id} onChange={onChange}  name="i3" type="text" className="form-control" placeholder="Add Description About Component" />
+                </div>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>{"       "}
+              <button value="Remove" className="btn btn-danger" onClick={handleRemove}>Remove</button>
+            </form>
           </div>
-          <button type="button" className="btn btn-primary" >Submit</button>{"       "}
-          <button value="Remove" className="btn btn-danger" onClick={handleRemove}>Remove</button>
-        </form>
+        </div>
       </div>
-    </div>
-  </div>
-    
   );
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
 export default App;
